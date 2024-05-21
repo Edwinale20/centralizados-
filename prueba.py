dafb214540cc411485a5cd807a9a60f242478d13
Edwinale20 <136374991+Edwinale20@users.noreply.github.com>
2024-05-21 13:45:05 -0600

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -8,7 +8,7 @@ import plotly.graph_objects as go
 
 
 # Ruta de la carpeta de OneDrive del usuario
-ruta_onedrive = r"\\CAI-ECARDENAG\Users\ecardenasg\OneDrive - ICONN\Documentos"
+ruta_onedrive = r"C:\Users\ecardenasg\OneDrive - ICONNs"
 
 # Paso 2: Subir el archivo semanal "centralizado BAT" desde la interfaz de Streamlit
 st.title("Carga y proceso de 'centralizado BAT'")
